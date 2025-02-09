57d3c80511bb5c6c3dc6f7e323030b79795a0941
Russell Johnson <16086475+RussellJQA@users.noreply.github.com>
2025-02-08 20:31:16 -0500

    Update standalone test to use arrow functions for loctors

--- tests/example2-mine-standalone2.spec.ts
+++ tests/example2-mine-standalone2.spec.ts
@@ -1,11 +1,19 @@
-import { test, expect, type Page } from "@playwright/test";
+import { test, expect, type Page, type Locator } from "@playwright/test";
 
-// This is my 1st refactoring of example.spec.ts, still keeping it as a standalone file (without using page objects) 
+// This is my 2nd refactoring of example.spec.ts, still keeping it as a standalone file (without using page objects),
+// but factoring out the locators as arrow functions, following the example of:
+// 	https://github.com/engenious-inc/playwright-examples/blob/advance-playwright/3-module/3.C-lecture/tests/example.spec.ts
 
 const baseURL = "https://playwright.dev/";
 const pageURL = /.*intro/;
 const javaDescription = `Playwright is distributed as a set of Maven modules. The easiest way to use it is to add one dependency to your project's pom.xml as described below. If you're not familiar with Maven please refer to its documentation.`;
 const nodeDescription = "Installing Playwright";
+
+// Locators
+const nodeLink = (page: Page): Locator => page.getByRole("button", { name: "Node.js" });
+const javaLink = (page: Page): Locator => page.getByRole("navigation", { name: "Main" }).getByText("Java");
+const nodeLabel = (page: Page): Locator => page.getByText(nodeDescription, { exact: true });
+const javaLabel = (page: Page): Locator => page.getByText(javaDescription);
 
 async function clickGetStarted(page: Page) {
   await page.getByRole("link", { name: "Get started" }).click();
@@ -16,25 +24,19 @@ async function assertPageUrl(page: Page, pageUrl: RegExp) {
 }
 
 async function hoverNode(page: Page) {
-  const nodeLink = page.getByRole("button", { name: "Node.js" });
-  await nodeLink.hover();
+  await nodeLink(page).hover();
 }
 
 async function clickJava(page: Page) {
-  const javaLink = page
-    .getByRole("navigation", { name: "Main" })
-    .getByText("Java");
-  await javaLink.click();
+  await javaLink(page).click();
 }
 
 async function assertNodeDescriptionNotVisible(page: Page) {
-  const nodeLabel = page.getByText(nodeDescription, { exact: true });
-  await expect(nodeLabel).not.toBeVisible();
+  await expect(nodeLabel(page)).not.toBeVisible();
 }
 
 async function assertJavaDescriptionVisible(page: Page) {
-  const javaLabel = page.getByText(javaDescription);
-  await expect(javaLabel).toBeVisible();
+  await expect(javaLabel(page)).toBeVisible();
 }
 
 test.describe('Test Playwright.dev home page', () => {
@@ -54,7 +56,7 @@ test.describe('Test Playwright.dev home page', () => {
 
   test("check Java page", async ({ page }) => {
     await clickGetStarted(page);
-    await assertPageUrl(page, pageURL);
+    await assertPageUrl(page, pageURL);  
     await page.waitForLoadState("load");
     await page.waitForLoadState("domcontentloaded");
     await hoverNode(page);
@@ -64,5 +66,4 @@ test.describe('Test Playwright.dev home page', () => {
     await assertNodeDescriptionNotVisible(page);
     await assertJavaDescriptionVisible(page);
   });
-
 });
